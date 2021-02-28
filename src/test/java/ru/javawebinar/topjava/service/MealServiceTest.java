0efa36e85326b16637dbb3fe460855e739878637
shem-m <65406861+shem-m@users.noreply.github.com>
2021-03-01 00:54:34 +0300

    HW4-opt add @Rule functionality

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +19,9 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.MealTestData.*;
@@ -26,6 +35,24 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
+    private static final Logger log = LoggerFactory.getLogger(MealServiceTest.class);
+
+    private static final List<String> WATCHER_LOG_LIST = new ArrayList<>();
+
+    @Rule
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String logTest = "Test " + description.getMethodName() + " was completed in " + TimeUnit.NANOSECONDS.toMillis(nanos) + " ms";
+            WATCHER_LOG_LIST.add(logTest);
+            log.info(logTest);
+        }
+    };
+
+    @AfterClass
+    public static void afterClass() {
+        WATCHER_LOG_LIST.forEach(log::info);
+    }
 
     @Autowired
     private MealService service;
